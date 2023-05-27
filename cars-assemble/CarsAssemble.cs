2433f9365ab7964adbbf159692f3d2171f81bd05
Long.nh <ng.hglong102@gmail.com>
2023-05-27 14:08:29 +0700

    Finished CarsAssemble

---
+++ cars-assemble/CarsAssemble.cs
@@ -0,0 +1,33 @@
+using System;
+
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if (speed == 0)
+        {
+            return 0;
+        }
+        else if (speed >= 1 && speed <= 4)
+            return 1;
+        else if (speed >= 5 && speed <= 8)
+            return 0.9;
+        else if (speed == 9)
+            return 0.8;
+        else
+            return 0.77;
+    }
+    
+    public static double ProductionRatePerHour(int speed)
+    {
+        double successRate = SuccessRate(speed);
+        int totalCarProduced = speed * 221;
+        return  totalCarProduced * successRate;
+
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        return (int)(ProductionRatePerHour(speed)/60);
+    }
+}
